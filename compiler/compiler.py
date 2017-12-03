a85be050ad70dd7a8f1aa6ee905ec2977b51102f
Jonathan David Page <jonathan@sleepingcyb.org>
2017-12-03 16:48:29 -0500

    Initial commit

---
+++ compiler/compiler.py
@@ -0,0 +1,183 @@
+import enum
+import io
+
+
+class EmptyNode:
+    def __repr__(self):
+        return "<empty>"
+
+
+class LetNode:
+    def __init__(self, )
+    def __iter__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+class Token(enum.Enum):
+    eof = -1
+    whitespace = 0
+    word = 1
+    numeric = 2
+    left_paren = 3
+    right_paren = 4
+    left_braket = 5
+    right_braket = 6
+    left_brace = 7
+    right_brace = 8
+    double_quote = 9
+    single_quote = 10
+    operator = 11
+    unknown = 12
+
+
+def production(node, n):
+    def decorate(f):
+        def fun(*args, **kwargs):
+            ns = tuple(f(*args, **kwargs))
+            if len(ns) == 0:
+                raise StopIteration
+            if len(ns) < n:
+                return ns[-1]
+            return node(*ns)
+        return fun
+    return decorate
+
+
+class Redo:
+    def __init__(self, source):
+        self.source = source
+        self.current = None
+        self.last = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.last is None:
+            self.current = next(self.source)
+        else:
+            self.current = self.last
+            self.last = None
+        return self.current
+
+    def redo(self):
+        self.last = self.current
+
+    def peek(self):
+        if self.last is None:
+            next(self)
+            self.redo()
+        return self.last
+
+
+class ParseError(Exception):
+    def __init__(self, message, pos):
+        super().__init__(message)
+        self.position = pos
+
+    def __str__(self):
+        msg = super().__str__()
+        return f"{msg} (at {self.position})"
+
+
+class Parser:
+    operators = [
+        '->', '<-', '=', '+=', '-=',
+        '+', '-', '*', '/', '|', '&', '^', '>>', '<<',
+        '!=', '==', '<=', '>=', '<', '>',
+        '/*', '*/'
+    ]
+
+    def __init__(self, source):
+        self.source = Redo(source)
+        self.scanner = Redo(self.scan())
+
+    def startsoperator(self, v):
+        return any(op for op in Parser.operators if op.startswith(v))
+
+    def scanrun(self, c, cond):
+        run = [c]
+        while True:
+            try:
+                c, _ = next(self.source)
+            except StopIteration:
+                break
+            run.append(c)
+            if not cond(c, "".join(run)):
+                self.source.redo()
+                run.pop()
+                break
+        return "".join(run)
+
+    def scan(self):
+        while True:
+            try:
+                c, position = next(self.source)
+            except StopIteration:
+                break
+            if self.startsoperator(c):
+                op = self.scanrun(c, lambda _, v: self.startsoperator(v))
+                # did we actually form an operator or just a prefix?
+                if op in Parser.operators:
+                    yield (Token.operator, op, position)
+                else:
+                    yield (Token.unknown, op, position)
+            elif c == "'":
+                yield (Token.single_quote, c, position)
+            elif c == '"':
+                yield (Token.double_quote, c, position)
+            elif c.isspace():
+                ws = self.scanrun(c, lambda v, _: v.isspace())
+                yield (Token.whitespace, ws, position)
+            elif c.isdigit():
+                num = self.scanrun(c, lambda v, _: not v.isspace())
+                yield (Token.numeric, num, position)
+            elif c.isalpha():
+                word = self.scanrun(c, lambda v, _: not v.isspace())
+                yield (Token.word, word, position)
+            else:
+                yield (Token.unknown, c, position)
+        while True:
+            # fuse the scanner
+            yield (Token.eof, None, None)
+
+    def matchtok(self, *toks):
+        return self.matchcond(lambda t, *_: t in toks)
+
+    def unmatchtok(self, *toks):
+        return self.matchcond(lambda t, *_: t not in toks)
+
+    def match(self, tok, val):
+        return self.matchcond(lambda t, v, _: t is tok and v == val)
+
+    def matchcond(self, cond):
+        if cond(*next(self.scanner)):
+            return True
+        self.scanner.redo()
+        return False
+
+    def parse(self):
+        while True:
+            yield self.statement()
+            if self.matchtok(Token.eof):
+                break
+
+    def anyof(self, *productions):
+        for p in productions:
+            try:
+                return p()
+            except StopIteration:
+                pass
+        raise StopIteration
+
+    def maybe(self, p):
+        try:
+            return p()
+        except StopIteration:
+            return None
+
+        def 
+
