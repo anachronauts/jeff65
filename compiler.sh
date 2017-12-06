d724ff967ced14f027dc343bc0a46a7d329ad578
Jonathan David Page <jonathan@sleepingcyb.org>
2017-12-05 23:26:43 -0500

    Reorganize code to be more jeff-centric

--- compiler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# if python3 is not available, we use 'python' and hope for the best
-if command -v python3 >/dev/null 2>&1
-then PYTHON=python3
-else PYTHON=python
-fi
-
-$PYTHON -m compiler $*
