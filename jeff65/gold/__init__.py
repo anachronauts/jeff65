7de22b7fbd382c7185090a6ae180243626ba97b2
Jonathan David Page <jonathan@sleepingcyb.org>
2017-12-06 00:55:44 -0500

    Move gold-syntax-related files to a submodule

--- jeff65/gold/__init__.py
+++ jeff65/gold/__init__.py
@@ -1,4 +1,4 @@
-# jeff65 main entry point
+# jeff65.gold module root
 # Copyright (C) 2017  jeff65 maintainers
 #
 # This program is free software: you can redistribute it and/or modify
@@ -13,23 +13,3 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-import argparse
-import sys
-
-from .gold import lexer, ast
-
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument("input_file", help="the file to compile")
-args = arg_parser.parse_args()
-
-
-def open_input(name):
-    if name == "-":
-        return sys.stdin
-    return open(name, 'r')
-
-
-with open_input(args.input_file) as input_file:
-    tree = ast.parse_all(lexer.lex(input_file))
-    print(tree)
