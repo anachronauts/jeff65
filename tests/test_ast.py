24b6a8ac84db515dd38cf4a7d91f5f701e2ee84c
Woodrow Barlow <wjbarlow@ncsu.edu>
2017-12-09 15:16:03 -0500

    Update tests

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -5,9 +5,22 @@ from jeff65.gold import ast, lexer
 
 def parse(source):
     with io.StringIO(source) as s:
-        a = ast.parse_all(lexer.lex(s))
+        l = list(lexer.lex(s))
+        a = ast.parse_all(l)
         assert_is_instance(a, ast.UnitNode)
         return a
+
+
+def test_empty_file():
+    a = parse("")
+    assert_equal(0, len(a.statements))
+
+
+def test_whitespace_only_file():
+    a = parse("\n")
+    assert_equal(1, len(a.statements))
+    c = a.statements[0]
+    assert_is_instance(c, ast.EofNode)
 
 
 def test_comments_newline():
@@ -24,8 +37,16 @@ def test_comments_no_newline():
     assert_is_instance(c, ast.EofNode)
 
 
+def test_nested_comment():
+    a = parse("--[[ a --[[ nested ]] comment ]]")
+    assert_equal(1, len(a.statements))
+    c = a.statements[0]
+    assert_is_instance(c, ast.EofNode)
+
+
 def test_comment_before_expression():
     a = parse("--[[ a comment ]] 1 + 2")
+    assert_equal(1, len(a.statements))
     c = a.statements[0]
     assert_is_instance(c, ast.OperatorAddNode)
     assert_is_instance(c.lhs, ast.NumericNode)
@@ -34,6 +55,7 @@ def test_comment_before_expression():
 
 def test_comment_after_expression():
     a = parse("1 + 2 --[[ a comment ]]")
+    assert_equal(1, len(a.statements))
     c = a.statements[0]
     assert_is_instance(c, ast.OperatorAddNode)
     assert_is_instance(c.lhs, ast.NumericNode)
@@ -42,15 +64,11 @@ def test_comment_after_expression():
 
 def test_comment_within_expression():
     a = parse("1 + --[[ a comment ]] 2")
+    assert_equal(1, len(a.statements))
     c = a.statements[0]
     assert_is_instance(c, ast.OperatorAddNode)
     assert_is_instance(c.lhs, ast.NumericNode)
     assert_is_instance(c.rhs, ast.NumericNode)
-
-def test_nested_comment():
-    a = parse("--[[ a --[[ nested ]] comment ]]")
-    c = a.statements[0]
-    assert_is_instance(c, ast.EofNode)
 
 
 def test_associativity():
@@ -78,6 +96,7 @@ def test_let_with_storage_class():
     assert_equal("a", b.lhs.lhs.text)
     assert_is_instance(b.lhs.rhs, ast.IdentifierNode)
     assert_equal("u8", b.lhs.rhs.text)
+
 
 def test_let_without_storage_class():
     a = parse("let a: u8 = 7")
