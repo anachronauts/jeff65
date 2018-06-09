6976eca0e37999ef5d3fc5c8432112b96892fe0a
Jonathan David Page <jonathan@sleepingcyb.org>
2018-06-09 17:16:49 -0400

    Add an 'objdump' subcommand (and move compilation to 'compile')

--- jeff65/__init__.py
+++ jeff65/__init__.py
@@ -16,21 +16,71 @@
 
 import argparse
 import pathlib
+import sys
 from . import gold
 from . import blum
 
 
 def main():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("input_file", help="the file to compile")
-    arg_parser.add_argument("-v", "--verbose",
-                            help="show the output of each pass",
-                            dest="verbose", action="store_true", default=False)
-    args = arg_parser.parse_args()
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
 
-    input_file = pathlib.PurePath(args.input_file)
+    compile_parser = subparsers.add_parser(
+        'compile', help="compile one or more files")
+    compile_parser.add_argument('file', help="the file to compile",
+                                type=pathlib.PurePath)
+    compile_parser.add_argument("-v", "--verbose",
+                                help="show the output of each pass",
+                                dest="verbose", action="store_true",
+                                default=False)
+    compile_parser.set_defaults(func=cmd_compile)
 
-    archive = gold.translate(input_file, args.verbose)
-    archive.dumpf(input_file.with_suffix('.blum'))
-    blum.link('{}.main'.format(input_file.stem), archive,
-              input_file.with_suffix('.prg'))
+    objdump_parser = subparsers.add_parser(
+        'objdump', help="list symbols of an object")
+    objdump_parser.add_argument('file', help="the file to examine",
+                                type=pathlib.PurePath)
+    objdump_parser.set_defaults(func=cmd_objdump)
+
+    args = parser.parse_args()
+
+    if hasattr(args, 'func'):
+        args.func(args)
+    else:
+        parser.print_help()
+        sys.exit(1)
+
+
+def cmd_none(args):
+    raise NotImplementedError
+
+
+def cmd_compile(args):
+    archive = gold.translate(args.file, args.verbose)
+    archive.dumpf(args.file.with_suffix('.blum'))
+    blum.link('{}.main'.format(args.file.stem), archive,
+              args.file.with_suffix('.prg'))
+
+
+def cmd_objdump(args):
+    archive = blum.Archive()
+    archive.loadf(args.file)
+
+    print("Unit: '{}'".format(args.file.stem))
+
+    if len(archive.constants) == 0:
+        print("Constants: (none)")
+    else:
+        print("Constants:")
+        for name, constant in archive.constants.items():
+            print("    {}: {}".format(name, constant))
+    print()
+
+    if len(archive.symbols) == 0:
+        print("Symbols: (none)")
+    else:
+        print("Symbols:")
+        for name, symbol in archive.symbols.items():
+            print("    {}: size={}, section={}, type={}".format(
+                name, len(symbol.data), symbol.section, symbol.type_info))
+            for relocation in symbol.relocations:
+                print("      {}".format(relocation))
