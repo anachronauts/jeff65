2b92f3e13bce5a56967636b2f4279897a30625a7
Jonathan David Page <jonathan@sleepingcyb.org>
2018-08-29 01:58:14 -0400

    Fill out enough metadata in setup.py/cfg to publish (#65)
    I've gone ahead and published a version 0.0.1 on PyPI in order to claim
    the name.

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='jeff65',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        'attrs>=18.1.0',
-    ],
+setuptools.setup(
     entry_points={
         'console_scripts': [
             'jeff65 = jeff65:main'
