f197f2a855cf95ceb7fc9e69623db50bf670ca9c
Jonathan David Page <jonathan@sleepingcyb.org>
2019-03-23 13:22:21 -0400

    [docs] RTD Fixes

--- doc/conf.py
+++ doc/conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'jeff65'
-copyright = '2019, Jonathan David Page'
-author = 'Jonathan David Page'
+copyright = '2019, Jonathan David Page & Woodrow Barlow'
+author = 'Jonathan David Page & Woodrow Barlow'
 
 # The short X.Y version
 version = ''
@@ -75,7 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -130,7 +130,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'jeff65.tex', 'jeff65 Documentation',
-     'Jonathan David Page', 'manual'),
+     'Jonathan David Page \and Woodrow Barlow', 'manual'),
 ]
 
 
